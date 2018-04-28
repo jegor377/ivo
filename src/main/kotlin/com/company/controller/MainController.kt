1fe4be40c224d827bdaf33d94cb2ead89620a46a
PSTR0NG <jegor377@gmail.com>
2018-04-28 12:30:59 +0200

    updating kotlin to 1.2 v.

--- src/main/kotlin/com/company/controller/MainController.kt
+++ src/main/kotlin/com/company/controller/MainController.kt
@@ -1,9 +1,14 @@
 package com.company.controller
 
+import javafx.scene.image.ImageView
 import tornadofx.*
 import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainController: Controller() {
+    lateinit var dirEntries: Array<File>
+    lateinit var headingImage: File
     val imagePath: String?
         get() {
             if(app.parameters.raw.size==1) {
@@ -16,4 +21,20 @@ class MainController: Controller() {
 
     private fun getTitle(size: Int): String =
             if(size == 1) ("IVO - " + app.parameters.raw[0]) else "IVO"
+
+    fun loadDirEntries() {
+        val file = File(app.parameters.raw[0])
+        if(file.isFile) {
+            val fileDir = file.parentFile
+            dirEntries = fileDir.listFiles()
+        }
+    }
+
+    fun nextImage(imgView: ImageView) {
+
+    }
+
+    fun prevImage(imgView: ImageView) {
+
+    }
 }
--- src/main/kotlin/com/company/view/MainView.kt
+++ src/main/kotlin/com/company/view/MainView.kt
@@ -6,12 +6,11 @@ import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.control.ContentDisplay
 import javafx.scene.effect.DropShadow
-import javafx.scene.image.Image
+import javafx.scene.image.ImageView
 import javafx.scene.input.MouseEvent
-import javafx.scene.layout.Background
-import javafx.scene.layout.BorderPane
 import javafx.scene.paint.Color
 import javafx.scene.paint.Paint
+import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class MainView : View() {
@@ -19,91 +18,112 @@ class MainView : View() {
     private lateinit var leftBtn: Button
     private lateinit var rightBtn: Button
     private lateinit var fullscreenBtn: Button
+    private lateinit var imgView: ImageView
 
     override val root = stackpane {
-        borderpane {
-            center {
-                if(controller.imagePath != null) imageview(controller.imagePath) {
-                    fitWidthProperty().bind(primaryStage.widthProperty())
-                    fitHeightProperty().bind(primaryStage.heightProperty())
-                    isPreserveRatio = true
+        if(controller.imagePath != null) {
+            borderpane {
+                center {
+                    imgView = imageview(controller.imagePath) {
+                        fitWidthProperty().bind(primaryStage.widthProperty())
+                        fitHeightProperty().bind(primaryStage.heightProperty())
+                        isPreserveRatio = true
+                    }
                 }
+                useMaxHeight = true
+                useMaxWidth = true
             }
-            useMaxHeight = true
-            useMaxWidth = true
-        }
-        borderpane {
-            left {
-                borderpane {
-                    center {
-                        leftBtn = button {
-                            graphic = svgpath("M23,0 L0,15 L23,30 L23,0 Z") {
+            borderpane {
+                left {
+                    borderpane {
+                        center {
+                            leftBtn = button {
+                                graphic = svgpath("M23,0 L0,15 L23,30 L23,0 Z") {
+                                    stroke = Color.WHITE
+                                    strokeWidth = 1.5
+                                    fill = Color.WHITE
+                                    effect = DropShadow()
+                                }
+                                action {
+                                    controller.prevImage(imgView)
+                                }
+                                contentDisplay = ContentDisplay.GRAPHIC_ONLY
+                                setMaxSize(0.0, 0.0)
+                                setMinSize(0.0, 0.0)
+                            }
+                            style {
+                                paddingLeft = 50.0
+                            }
+                        }
+                    }
+                }
+                right {
+                    borderpane {
+                        center {
+                            rightBtn = button {
+                                graphic = svgpath("M0,0 L23,15 L0,30 L0,0 Z") {
+                                    stroke = Color.WHITE
+                                    strokeWidth = 1.5
+                                    fill = Color.WHITE
+                                    effect = DropShadow()
+                                }
+                                action {
+                                    controller.nextImage(imgView)
+                                }
+                                contentDisplay = ContentDisplay.GRAPHIC_ONLY
+                                setMaxSize(0.0, 0.0)
+                                setMinSize(0.0, 0.0)
+                            }
+                            style {
+                                paddingRight = 50.0
+                            }
+                        }
+                    }
+                }
+                center {
+                    fullscreenBtn = button {
+                        graphic = group {
+                            /*circle {
+                                centerX = 10.0
+                                centerY = 10.0
+                                radius = 28.0
+                                fill = Color.BLACK
+                            }*/
+                            svgpath("M0,5 L0,0 L5,0 M15,0 L20,0 L20,5 M20,15 L20,20 L15,20 M5,20 L0,20 L0,15") {
                                 stroke = Color.WHITE
                                 strokeWidth = 1.5
-                                fill = Color.WHITE
+                                fill = Color.TRANSPARENT
+                                scaleX = 2.0
+                                scaleY = 2.0
+                                effect = DropShadow()
                             }
-                            contentDisplay = ContentDisplay.GRAPHIC_ONLY
-                            setMaxSize(0.0, 0.0)
-                            setMinSize(0.0, 0.0)
+                            opacity = 0.0
                         }
-                        style {
-                            paddingLeft = 20.0
+                        action {
+                            primaryStage.isFullScreen = !primaryStage.isFullScreen
                         }
+                        onHover {
+                            fullscreenBtn.graphic.opacity = 1.0
+                        }
+                        onMouseExited = EventHandler<MouseEvent> {
+                            fullscreenBtn.graphic.opacity = 0.0
+                        }
+                        contentDisplay = ContentDisplay.GRAPHIC_ONLY
+                        setMaxSize(0.0, 0.0)
+                        setMinSize(0.0, 0.0)
                     }
                 }
             }
-            right {
-                borderpane {
-                    center {
-                        rightBtn = button {
-                            graphic = svgpath("M0,0 L23,15 L0,30 L0,0 Z") {
-                                stroke = Color.WHITE
-                                strokeWidth = 1.5
-                                fill = Color.WHITE
-                            }
-                            contentDisplay = ContentDisplay.GRAPHIC_ONLY
-                            setMaxSize(0.0, 0.0)
-                            setMinSize(0.0, 0.0)
-                        }
-                        style {
-                            paddingRight = 20.0
-                        }
-                    }
-                }
-            }
-            center {
-                fullscreenBtn = button {
-                    graphic = group {
-                        /*circle {
-                            centerX = 10.0
-                            centerY = 10.0
-                            radius = 28.0
-                            fill = Color.BLACK
-                        }*/
-                        svgpath("M0,5 L0,0 L5,0 M15,0 L20,0 L20,5 M20,15 L20,20 L15,20 M5,20 L0,20 L0,15") {
-                            stroke = Color.WHITE
-                            strokeWidth = 1.5
-                            fill = Color.TRANSPARENT
-                        }
-                        effect = DropShadow()
-                        opacity = 0.0
-                    }
-                    action {
-                        primaryStage.isFullScreen = !primaryStage.isFullScreen
-                    }
-                    onHover {
-                        fullscreenBtn.graphic.opacity = 1.0
-                    }
-                    onMouseExited = EventHandler<MouseEvent> {
-                        fullscreenBtn.graphic.opacity = 0.0
-                    }
-                    contentDisplay = ContentDisplay.GRAPHIC_ONLY
-                    setMaxSize(0.0, 0.0)
-                    setMinSize(0.0, 0.0)
+            alignment = Pos.TOP_LEFT
+        } else {
+            label("IVO") {
+                style {
+                    textFill = Paint.valueOf("444")
+                    fontSize = 72.px
+                    fontWeight = FontWeight.BOLD
                 }
             }
         }
-        alignment = Pos.TOP_LEFT
         style {
             backgroundColor += Paint.valueOf("222")
         }
